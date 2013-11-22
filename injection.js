5c788651b72ffefe6635f45b14b565363de6d942
Peter Hayes <peter.k.hayes@gmail.com>
2013-11-22 11:56:49 -0800

    some changes to syntax.  can define goals.  experiences saved in object

--- injection.js
+++ injection.js
@@ -1,10 +1,31 @@
+var experiences = {};
+
 // Live NodeList
-var ab_tests = document.getElementsByTagName('ab');
+var abTests = document.getElementsByTagName('abtest');
 
 // ab mutates as we replace its nodes
-while (ab_tests.length) {
-  var children = ab_tests[0].children;
-  var exp_number = ~~(Math.random()*children.length);
+while (abTests.length) {
+  // Define variables.
+  var current = abTests[0];
+  var children = current.children;
+  var expNumber = ~~(Math.random()*children.length);
+  var selectedChild = children[expNumber];
 
-  ab_tests[0].parentNode.replaceChild(children[exp_number], ab_tests[0]);
+  // Save the history of the test.
+  var testName = current.getAttribute('test-name');
+  var expName = selectedChild.getAttribute('exp-name');
+  experiences[testName] = expName;
+
+  selectedChild.removeAttribute('exp-name');
+  current.parentNode.replaceChild(selectedChild, current);
+}
+
+var abGoals = document.getElementsByTagName('abgoal');
+
+for (var i = 0; i < abGoals.length; i++) {
+  var goal = abGoals[i];
+  var goalName = goal.getAttribute('goal-name');
+  goal.addEventListener('click', function() { console.log('clicked ' + goalName, experiences); }, false);
+  selectedChild.removeAttribute('goal-name');
+
 }
