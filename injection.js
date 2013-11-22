65996836a14ece074cf47e67ee4c6c9ee3cf7782
Rich Parrish <richparrishx@gmail.com>
2013-11-22 10:46:22 -0800

    Comments & consistency

--- injection.js
+++ injection.js
@@ -1,5 +1,7 @@
+// Live NodeList
 var ab_tests = document.getElementsByTagName('ab');
 
+// ab mutates as we replace its nodes
 while (ab_tests.length) {
   var children = ab_tests[0].children;
   var exp_number = ~~(Math.random()*children.length);
