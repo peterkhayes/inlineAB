c3d351aa0fd0c6967b1002bc79ba8d1f9bbed8d4
Rich Parrish <richparrishx@gmail.com>
2013-11-25 21:07:56 -0800

    add helpers

--- auth.js
+++ auth.js
@@ -1,57 +1,342 @@
+/**
+ * Authorization information. This should be obtained through the Google APIs
+ * developers console. https://code.google.com/apis/console/
+ * Also there is more information about how to get these in the authorization
+ * section in the Google JavaScript Client Library.
+ * https://code.google.com/p/google-api-javascript-client/wiki/Authentication
+ */
+
+var clientId = '657336628940-qfgikt5qv8k8ervasdta1orcdjgp6n5m.apps.googleusercontent.com';
+var scopes = 'https://www.googleapis.com/auth/analytics.readonly';
+var apiKey = 'AIzaSyBqBRgHijo2L3Ezbwu_DsEVzQRTL5oVpg8';
+
+/**
+ * Callback executed once the Google APIs Javascript client library has loaded.
+ * The function name is specified in the onload query parameter of URL to load
+ * this library. After 1 millisecond, checkAuth is called.
+ */
+function handleClientLoad() {
+  gapi.client.setApiKey(apiKey);
+  window.setTimeout(checkAuth, 1);
+}
+
+/**
+ * Uses the OAuth2.0 clientId to query the Google Accounts service
+ * to see if the user has authorized. Once complete, handleAuthResults is
+ * called.
+ */
+function checkAuth() {
+  gapi.auth.authorize({
+    client_id: clientId, scope: scopes, immediate: true}, handleAuthResult);
+}
+
+/**
+ * Handler that is called once the script has checked to see if the user has
+ * authorized access to their Google Analytics data. If the user has authorized
+ * access, the analytics api library is loaded and the handleAuthorized
+ * function is executed. If the user has not authorized access to their data,
+ * the handleUnauthorized function is executed.
+ * @param {Object} authResult The result object returned form the authorization
+ *     service that determine whether the user has currently authorized access
+ *     to their data. If it exists, the user has authorized access.
+ */
+function handleAuthResult(authResult) {
+  if (authResult) {
+    gapi.client.load('analytics', 'v3', handleAuthorized);
+  } else {
+    handleUnauthorized();
+  }
+}
+
+/**
+ * Updates the UI once the user has authorized this script to access their
+ * data. This changes the visibiilty on some buttons and adds the
+ * makeApiCall click handler to the run-demo-button.
+ */
+function handleAuthorized() {
+  var authorizeButton = document.getElementById('authorize-button');
+  var runDemoButton = document.getElementById('run-demo-button');
+
+  authorizeButton.style.visibility = 'hidden';
+  runDemoButton.style.visibility = '';
+  runDemoButton.onclick = makeApiCall;
+  outputToPage('Click the Run Demo button to begin.');
+}
+
+/**
+ * Updates the UI if a user has not yet authorized this script to access
+ * their Google Analytics data. This function changes the visibility of
+ * some elements on the screen. It also adds the handleAuthClick
+ * click handler to the authorize-button.
+ */
+function handleUnauthorized() {
+  var authorizeButton = document.getElementById('authorize-button');
+  var runDemoButton = document.getElementById('run-demo-button');
+
+  runDemoButton.style.visibility = 'hidden';
+  authorizeButton.style.visibility = '';
+  authorizeButton.onclick = handleAuthClick;
+  outputToPage('Please authorize this script to access Google Analytics.');
+}
+
+/**
+ * Handler for clicks on the authorization button. This uses the OAuth2.0
+ * clientId to query the Google Accounts service to see if the user has
+ * authorized. Once complete, handleAuthResults is called.
+ * @param {Object} event The onclick event.
+ */
+function handleAuthClick(event) {
+  gapi.auth.authorize({
+    client_id: clientId, scope: scopes, immediate: false}, handleAuthResult);
+  return false;
+}
+
 function auth() {
   var config = {
     'response_type': 'token',
     'approval_prompt': 'auto',
-    'client_id': '657336628940-qfgikt5qv8k8ervasdta1orcdjgp6n5m.apps.googleusercontent.com',
-    'scope': 'https://www.googleapis.com/auth/analytics.readonly'
+    'client_id': clientId,
+    'scope': scopes
   };
   gapi.auth.authorize(config, function(res) {
     console.log('login complete');
     console.log(gapi.auth.getToken());
     console.log(res);
-    cb(res);
+    validate(res.access_token);
   });
 }
 
-function cb() {
-  // Parse the query string
-  var params = {}, 
-      queryString = location.hash.substring(1),
-      regex = /([^&=]+)=([^&]*)/g, 
-      m;
-
-  while (m = regex.exec(queryString)) {
-    params[decodeURIComponent(m[1])] = decodeURIComponent(m[2]);
-  }
-
-  // Send the token to the server
-  var xhr = new XMLHttpRequest();
-
-  xhr.open('GET', 'https://' + window.location.host + '/catchtoken?' + queryString, true);
-  xhr.onreadystatechange = function(e) {
-    if (xhr.readyState === 4) {
-       if(xhr.status === 200){
-        console.log('success!');
-         window.location = params['state'];
-     }
-    else if(xhr.status === 400) {
-          alert('There was an error processing the token.');
-      }
-      else {
-        alert('something else other than 200 was returned');
-      }
-    }
-  };
-  xhr.send(null);
+/**
+ * Executes a query to the Management API to retrieve all the users accounts.
+ * Once complete, handleAccounts is executed. Note: A user must have gone
+ * through the Google APIs authorization routine and the Google Anaytics
+ * client library must be loaded before this function is called.
+ */
+function makeApiCall() {
+  outputToPage('Querying Accounts.');
+  gapi.client.analytics.management.accounts.list().execute(handleAccounts);
 }
 
-function verify() {
+/**
+ * Handles the API response for querying the accounts collection. This checks
+ * to see if any error occurs as well as checks to make sure the user has
+ * accounts. It then retrieve the ID of the first account and then executes
+ * queryWebProeprties.
+ * @param {Object} response The response object with data from the
+ *     accounts collection.
+ */
+
+function handleAccounts(response) {
+  if (!response.code) {
+    if (response && response.items && response.items.length) {
+      var firstAccountId = response.items[0].id;
+      queryWebproperties(firstAccountId);
+    } else {
+      updatePage('No accounts found for this user.')
+    }
+  } else {
+    updatePage('There was an error querying accounts: ' + response.message);
+  }
+}
+
+/**
+ * Executes a query to the Management API to retrieve all the users
+ * webproperties for the provided accountId. Once complete,
+ * handleWebproperties is executed.
+ * @param {String} accountId The ID of the account from which to retrieve
+ *     webproperties.
+ */
+function queryWebproperties(accountId) {
+  updatePage('Querying Webproperties.');
+  gapi.client.analytics.management.webproperties.list({
+      'accountId': accountId
+  }).execute(handleWebproperties);
+}
+
+/**
+ * Handles the API response for querying the webproperties collection. This
+ * checks to see if any error occurs as well as checks to make sure the user
+ * has webproperties. It then retrieve the ID of both the account and the
+ * first webproperty, then executes queryProfiles.
+ * @param {Object} response The response object with data from the
+ *     webproperties collection.
+ */
+function handleWebproperties(response) {
+  if (!response.code) {
+    if (response && response.items && response.items.length) {
+      var firstAccountId = response.items[0].accountId;
+      var firstWebpropertyId = response.items[0].id;
+      queryProfiles(firstAccountId, firstWebpropertyId);
+    } else {
+      updatePage('No webproperties found for this user.')
+    }
+  } else {
+    updatePage('There was an error querying webproperties: ' +
+        response.message);
+  }
+}
+
+
+/**
+ * Executes a query to the Management API to retrieve all the users
+ * profiles for the provided accountId and webPropertyId. Once complete,
+ * handleProfiles is executed.
+ * @param {String} accountId The ID of the account from which to retrieve
+ *     profiles.
+ * @param {String} webpropertyId The ID of the webproperty from which to
+ *     retrieve profiles.
+ */
+function queryProfiles(accountId, webpropertyId) {
+  updatePage('Querying Profiles.');
+  gapi.client.analytics.management.profiles.list({
+    'accountId': accountId,
+    'webPropertyId': webpropertyId
+  }).execute(handleProfiles);
+}
+
+
+/**
+ * Handles the API response for querying the profiles collection. This
+ * checks to see if any error occurs as well as checks to make sure the user
+ * has profiles. It then retrieve the ID of the first profile and
+ * finally executes queryCoreReportingApi.
+ * @param {Object} response The response object with data from the
+ *     profiles collection.
+ */
+function handleProfiles(response) {
+  if (!response.code) {
+    if (response && response.items && response.items.length) {
+      var firstProfileId = response.items[0].id;
+      queryCoreReportingApi(firstProfileId);
+    } else {
+      updatePage('No profiles found for this user.')
+    }
+  } else {
+    updatePage('There was an error querying profiles: ' + response.message);
+  }
+}
+
+
+/**
+ * Execute a query to the Core Reporting API to retrieve the top 25
+ * organic search terms by visits for the profile specified by profileId.
+ * Once complete, handleCoreReportingResults is executed.
+ * @param {String} profileId The profileId specifying which profile to query.
+ */
+function queryCoreReportingApi(profileId) {
+  updatePage('Querying Core Reporting API.');
+  gapi.client.analytics.data.ga.get({
+    'ids': 'ga:' + profileId,
+    'start-date': lastNDays(14),
+    'end-date': lastNDays(0),
+    'metrics': 'ga:visits',
+    'dimensions': 'ga:source,ga:keyword',
+    'sort': '-ga:visits,ga:source',
+    'filters': 'ga:medium==organic',
+    'max-results': 25
+  }).execute(handleCoreReportingResults);
+}
+
+
+/**
+ * Handles the API reponse for querying the Core Reporting API. This first
+ * checks if any errors occured and prints the error messages to the screen.
+ * If sucessful, the profile name, headers, result table are printed for the
+ * user.
+ * @param {Object} response The reponse returned from the Core Reporting API.
+ */
+function handleCoreReportingResults(response) {
+  if (!response.code) {
+    if (response.rows && response.rows.length) {
+      var output = [];
+
+      // Profile Name.
+      output.push('Profile Name: ', response.profileInfo.profileName, '<br>');
+
+      var table = ['<table>'];
+
+      // Put headers in table.
+      table.push('<tr>');
+      for (var i = 0, header; header = response.columnHeaders[i]; ++i) {
+        table.push('<th>', header.name, '</th>');
+      }
+      table.push('</tr>');
+
+      // Put cells in table.
+      for (var i = 0, row; row = response.rows[i]; ++i) {
+        table.push('<tr><td>', row.join('</td><td>'), '</td></tr>');
+      }
+      table.push('</table>');
+
+      output.push(table.join(''));
+      outputToPage(output.join(''));
+    } else {
+      outputToPage('No results found.');
+    }
+  } else {
+    updatePage('There was an error querying core reporting API: ' +
+        response.message);
+  }
+}
+
+
+/**
+ * Utility method to update the output section of the HTML page. Used
+ * to output messages to the user. This overwrites any existing content
+ * in the output area.
+ * @param {String} output The HTML string to output.
+ */
+function outputToPage(output) {
+  document.getElementById('output').innerHTML = output;
+}
+
+
+/**
+ * Utility method to update the output section of the HTML page. Used
+ * to output messages to the user. This appends content to any existing
+ * content in the output area.
+ * @param {String} output The HTML string to output.
+ */
+function updatePage(output) {
+  document.getElementById('output').innerHTML += '<br>' + output;
+}
+
+
+/**
+ * Utility method to return the lastNdays from today in the format yyyy-MM-dd.
+ * @param {Number} n The number of days in the past from tpday that we should
+ *     return a date. Value of 0 returns today.
+ */
+function lastNDays(n) {
+  var today = new Date();
+  var before = new Date();
+  before.setDate(today.getDate() - n);
+
+  var year = before.getFullYear();
+
+  var month = before.getMonth() + 1;
+  if (month < 10) {
+    month = '0' + month;
+  }
+
+  var day = before.getDate();
+  if (day < 10) {
+    day = '0' + day;
+  }
+
+  return [year, month, day].join('-');
+}
+
+
+
+function validate(token) {
   var xhr = new XMLHttpRequest();
-  xhr.open('GET','https://www.googleapis.com/oauth2/v1/tokeninfo?access_token=1/fFBGRNJru1FQd44AzqT3Zg', true);
+  xhr.open('GET','https://www.googleapis.com/oauth2/v1/tokeninfo?access_token='+token, true);
   xhr.onreadystatechange = function(e){
     if(xhr.readyState === 4 && xhr.status === 200){
       console.log(e);
-      console.log('success!');
+      if(e.audience === clientId) console.log('ALMOST THERE!');
+      console.log('success second!');
     } else if(xhr.status === 400){
       console.log('server error');
     } else {
