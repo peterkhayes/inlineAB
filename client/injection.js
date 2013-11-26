5c162759c5b629957c66736e18e4feca0e3dc574
Rich Parrish <richparrishx@gmail.com>
2013-11-25 16:09:43 -0800

    Create custom HTML elements in IE 7/8 & cleanup var declarations. Remove node_modules.

--- client/injection.js
+++ client/injection.js
@@ -7,130 +7,129 @@ ga('create', 'UA-45967923-1', 'auto');
 
 (function(window) {
 
-  console.log(document.cookie);
+  console.log('Cookie: ', document.cookie);
 
+  // Add custom HTML tags for IE versions that are not 9 or 10+
+  if(navigator.appVersion.indexOf('MSIE 9') === -1
+    && navigator.appVersion.indexOf('MSIE 1') === -1){
+    document.createElement('abtest');
+    document.createElement('abclass');
+    document.createElement('abgoal');
+  } 
 
-// Check for cookie
-// If cookie,
-  // If !didNotUseCookie use GAID
-  // Else use saved exp (didNotUseCookie)
-// If no cookie, Math.random exp && save didNoteUseCookie = exp;
+  var abTests = document.getElementsByTagName('abtest'),
+      abClasses = document.getElementsByTagName('abclass'),
+      abGoals = document.getElementsByTagName('abgoal'),
+      testsSeen = '',
+      testData = {},
+      GAID,
+      timeout;
 
+  // Check for cookie
+  // If cookie,
+    // If !didNotUseCookie use GAID
+    // Else use saved exp (didNotUseCookie)
+  // If no cookie, Math.random exp && save didNoteUseCookie = exp;
 
-// Polyfills
-''.trim || (String.prototype.trim = function(){return this.replace(/^[\s\uFEFF]+|[\s\uFEFF]+$/g,'');});
+  // Polyfills
+  ''.trim || (String.prototype.trim = function(){return this.replace(/^[\s\uFEFF]+|[\s\uFEFF]+$/g,'');});
 
-var testsSeen = '';
+  // Google Analytics stuff.
+  var getGAID = function(key){
+    var result;
+    return (result = new RegExp('(?:^|; )' + encodeURIComponent(key) + '=([^;]*)').exec(document.cookie)) ? (result[1]) : null;
+  };
 
-// Google Analytics stuff.
-var getGAID = function(key){
-  var result;
-  return (result = new RegExp('(?:^|; )' + encodeURIComponent(key) + '=([^;]*)').exec(document.cookie)) ? (result[1]) : null;
-};
+  GAID = getGAID('_ga') || getGAID('__utma');
 
-var GAID = getGAID('_ga') || getGAID('__utma');
-var testData = {};
+  var hash = function(input){
+    input = (typeof input === 'string' ? input : input.toString());
+    var hash = 0, i, char;
+    if (input.length === 0) return hash;
+    for (i = 0, l = input.length; i < l; i++) {
+        char  = input.charCodeAt(i);
+        hash = hash & hash; // Convert to bitwise
+        hash  = ((hash>>>5)-hash)+char;
+        hash |= 0; // Convert to 32bit integer
+    }
+    return Math.abs(hash);
+  };
 
-var hash = function(input){
-  input = (typeof input === 'string' ? input : input.toString());
-  var hash = 0, i, char;
-  if (input.length === 0) return hash;
-  for (i = 0, l = input.length; i < l; i++) {
-      char  = input.charCodeAt(i);
-      hash = hash & hash; // Convert to bitwise
-      hash  = ((hash>>>5)-hash)+char;
-      hash |= 0; // Convert to 32bit integer
-  }
-  return Math.abs(hash);
-};
+  var getExpNumber = function(testName, numberOfExperiences) {
+    var hashed = hash(testName + GAID);
+    var ans = (hashed % numberOfExperiences);
+    return ans;
+  };
 
-var getExpNumber = function(testName, numberOfExperiences) {
-  var hashed = hash(testName + GAID);
-  var ans = (hashed % numberOfExperiences);
-  return ans;
-};
+  var substitute = function() {
+    console.log('sub');
+    // ab mutates as we replace its nodes
+    while (abTests.length) {
+      // Define variables.
+      var currentTest = abTests[0];
+      var testName = currentTest.getAttribute('test-name');
+      var expName = currentTest.getAttribute('exp-name');
+      testsSeen += '&.t' + testName + '__' + expName;
+      var experiences = currentTest.children;
+      var expNumber = getExpNumber(testName, experiences.length);
+      var selectedExperience = experiences[expNumber];
 
-var abTests = document.getElementsByTagName('abtest');
-var abClasses = document.getElementsByTagName('abclass');
-var abGoals = document.getElementsByTagName('abgoal');
+      // Save the history of the test.
+      testData[testName] = selectedExperience.getAttribute('exp-name');
 
+      // Clean up the DOM.
+      selectedExperience.removeAttribute('exp-name');
+      currentTest.parentNode.replaceChild(selectedExperience, currentTest);
+    }
 
-var substitute = function() {
-  console.log('sub');
-  // Live NodeList
+    while (abClasses.length) {
+      var currentClassTest = abClasses[0];
+      var elem = currentClassTest.children[0];
+      var classTestName = currentClassTest.getAttribute('test-name');
+      var classOptions = currentClassTest.getAttribute('test-classes').split('|');
+      var classExpNumber = getExpNumber(classTestName, classOptions.length);
+      var selectedClass = classOptions[classExpNumber].trim();
+      testsSeen += '&.c' + classTestName + '__' + selectedClass;
+      elem.className += (' ' + selectedClass);
 
-  // ab mutates as we replace its nodes
-  while (abTests.length) {
-    // Define variables.
-    var currentTest = abTests[0];
-    var testName = currentTest.getAttribute('test-name');
-    var expName = currentTest.getAttribute('exp-name');
-    testsSeen += '&.t' + testName + '__' + expName;
-    var experiences = currentTest.children;
-    var expNumber = getExpNumber(testName, experiences.length);
-    var selectedExperience = experiences[expNumber];
+      // Save the history of the test.
+      testData[classTestName] = classOptions[classExpNumber].trim();
 
-    // Save the history of the test.
-    testData[testName] = selectedExperience.getAttribute('exp-name');
+      // Clean up the DOM.
+      currentClassTest.parentNode.replaceChild(elem, currentClassTest);
+    }
 
-    // Clean up the DOM.
-    selectedExperience.removeAttribute('exp-name');
-    currentTest.parentNode.replaceChild(selectedExperience, currentTest);
-  }
+    while(abGoals.length) {
+      var goal = abGoals[0];
+      var goalName = goal.getAttribute('goal-name');
+      var goalTarget = goal.children[0];
 
-  while (abClasses.length) {
-    var currentClassTest = abClasses[0];
-    var elem = currentClassTest.children[0];
-    var classTestName = currentClassTest.getAttribute('test-name');
-    var classOptions = currentClassTest.getAttribute('test-classes').split('|');
-    var classExpNumber = getExpNumber(classTestName, classOptions.length);
-    var selectedClass = classOptions[classExpNumber].trim();
-    testsSeen += '&.c' + classTestName + '__' + selectedClass;
-    elem.className += (' ' + selectedClass);
+      // Clean up the DOM.
+      goalTarget.addEventListener('click', function() { console.log('clicked ' + goalName, testData); }, false);
+      goal.parentNode.replaceChild(goalTarget, goal);
+    }
+  };
 
-    // Save the history of the test.
-    testData[classTestName] = classOptions[classExpNumber].trim();
+  timeout = setInterval(substitute, 20);
 
-    // Clean up the DOM.
-    currentClassTest.parentNode.replaceChild(elem, currentClassTest);
-  }
+  document.addEventListener('DOMContentLoaded', function() {
+    console.log('Dom content loaded');
+    // clearTimeout(timeout);
+    
+    // Send custom attributes held in testsSeen
+    ga('send', {
+      'hitType': 'pageview',
+      'title': testsSeen
+    });
 
-  while(abGoals.length) {
-    var goal = abGoals[0];
-    var goalName = goal.getAttribute('goal-name');
-    var goalTarget = goal.children[0];
+  }, false);
 
-    // Clean up the DOM.
-    goalTarget.addEventListener('click', function() { console.log('clicked ' + goalName, testData); }, false);
-    goal.parentNode.replaceChild(goalTarget, goal);
-  }
-};
+  // var swizzle = function() {
+  //   substitute();
+  //   timeout = setTimeout(swizzle, 1);
+  // };
 
-var timeout;
-
-timeout = setInterval(substitute, 20);
-
-document.addEventListener('DOMContentLoaded', function() {
-  console.log("Dom content loaded");
-  // clearTimeout(timeout);
-  
-  // Send custom attributes held in testsSeen
-  ga('send', {
-    'hitType': 'pageview',
-    'title': testsSeen
-  });
-
-  // substitute();
-}, false);
-
-// var swizzle = function() {
-//   substitute();
-//   timeout = setTimeout(swizzle, 1);
-// };
-
-// swizzle();
-
-
+  // swizzle();
 
 })(window);
 
