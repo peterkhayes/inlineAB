45a8199c03546f1b9681ca4f03e268719f43ceac
Joey Yang <joey@joeyyang.com>
2013-12-02 14:24:45 -0800

    submit fix for goalActions failing on (bang)goal.getAttribute('goal-action')

--- inlineab.js
+++ inlineab.js
@@ -10,16 +10,6 @@
 // The file is inserted directly after the opening <body> tag in the page's HTML.
 //
 
-// Standard Google Universal Analytics setup code.
-(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-})(window,document,'script','//www.google-analytics.com/analytics.js','ga');
-
-// Set second argument with your GA Tracking ID.
-// This can be found under Admin > Property > Property Settings > Tracking ID
-ga('create', 'UA-45967923-1', 'auto');
-
 (function(window) {
 
   // Add custom HTML tags for IE versions that are not 9 or 10+
@@ -165,7 +155,7 @@ ga('create', 'UA-45967923-1', 'auto');
       var goal = abGoals[0];
       var goalName = goal.getAttribute('goal-name').trim();
       var goalTarget = goal.children[0];
-      var goalActions = goal.getAttribute('goal-action').split(',') || ['click'];
+      var goalActions = goal.getAttribute('goal-action') ? goal.getAttribute('goal-action').split(',') : ['click'];
 
       // clean extra spaces from string
       for (var i = 0; i < goalActions.length; i++) {
